e63f7304ecbed39a08558a355f26e610f102a0e7
afzytk <afzytk@gmail.com>
2025-10-27 16:54:57 +0530

    feat(pages): add Home and Favorite pages chore(app): remove Vite and React SVG imports from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
   return (
     <>
-    
+    <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
+    <MovieCard movie={{title: "Avatar", release_date: "2024"}}/>
+    <MovieCard movie={{title: "Jurassic Park", release_date: "2024"}}/>
     </>
   )
 }
