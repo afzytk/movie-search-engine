fce535b58f913936069d91b0a4eb97587d071064
afzytk <afzytk@gmail.com>
2025-11-11 19:00:14 +0530

    feat(Favorites): render favorite movies using MovieCard component

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,11 +1,28 @@
 import "../css/Favorites.css"
+import { useMovieContext } from "../context/MovieContext"
+import MovieCard from "../components/MovieCard"
 
 function favorites() {
-    return (
-        <div>
-            <h1>Favorite Page</h1>
-        </div>
-    )
+    const {favorites} = useMovieContext();
+
+    if (favorites) {
+        return (
+            <div className="favorites">
+               <h2>Your Favorites</h2>
+            <div className="movies-grid">
+                {favorites.map((movie) => (
+                 <MovieCard movie={movie} key={movie.id} />
+                ))}
+               </div>
+            </div>
+            );
+}
+
+return ( <div className="favorites-empty">
+    <h2>No Favorite movies yet</h2>
+    <p>start adding movies to your favorites and they will appear here</p>
+</div>
+);
 }
 
 export default favorites
